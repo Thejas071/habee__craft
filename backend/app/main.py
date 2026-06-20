863abb1dd41a614d9b6e04d86fcd0aa13101af88
Vinayaka S <186694354+vinayaka2003@users.noreply.github.com>
2026-06-20 22:45:17 +0530

    Fix CORS for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,7 +32,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://habee-craft.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -56,9 +57,9 @@ app.mount(
     name="uploads"
 )
 
-
 @app.get("/")
 def root():
     return {
         "message": "Habee Craft API Running"
     }
+    
